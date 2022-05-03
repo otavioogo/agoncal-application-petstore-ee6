09a5b91d2b943accbfc1e634ab3e8bfac99e40de
Otavio Ogo <79700599+otavioogo@users.noreply.github.com>
2022-05-03 01:24:57 -0300

    Utilizando  @EqualsAndHashCode
    Método equals e hashCode são gerados automaticamente para os campos do objeto

--- src/main/java/org/agoncal/application/petstore/domain/CartItem.java
+++ src/main/java/org/agoncal/application/petstore/domain/CartItem.java
@@ -2,13 +2,14 @@ package org.agoncal.application.petstore.domain;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author Antonio Goncalves
  *         http://www.antoniogoncalves.org
  *         --
  */
-
+    @EqualsAndHashCode
 public class CartItem {
 
     // ======================================
@@ -61,27 +62,6 @@ public class CartItem {
     // ======================================
     // =   Methods hash, equals, toString   =
     // ======================================
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        CartItem cartItem = (CartItem) o;
-
-        if (!item.equals(cartItem.item)) return false;
-        if (!quantity.equals(cartItem.quantity)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = item.hashCode();
-        result = 31 * result + quantity.hashCode();
-        return result;
-    }
 
     @Override
     public String toString() {
